56e28cc0f995705da243e577c8311e532cf6f624
NoFox <nfox598625@gmail.com>
2024-02-06 20:42:22 -0500

    reworked install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,6 +3,7 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
   window.deferredPrompt = event;
   butInstall.classList.toggle("hidden", false);
 });
@@ -22,5 +23,6 @@ butInstall.addEventListener("click", async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
+  console.log("installed!");
   window.deferredPrompt = null;
 });
